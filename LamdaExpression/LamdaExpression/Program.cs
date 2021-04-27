6b87d07de7e48f41969987225c44880fd1e7654a
unknown <nilimawadal1@gmail.com>
2021-04-27 18:31:58 +0530

    [Nilima]Added Retrive TeenAge

--- LamdaExpression/LamdaExpression/Program.cs
+++ LamdaExpression/LamdaExpression/Program.cs
@@ -10,46 +10,72 @@ namespace LamdaExpression
 /// </summary>
     class Program
     {/// <summary>
-     /// 
+     /// Retrieve teenage
      /// </summary>
      /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("*******Welcome to Lambda Expressions*********");
+            //Creating Listof PersonClass
             List<PersonClass> list = new List<PersonClass>();
-            IterateOverLoop(list);
-            AddingPerson(list);
-            RetrieveTop2(list);
+            IterateOverLoop(list);//Calling IterateOverLoop
+            AddingPerson(list);//CAlling AddingPerson
+            RetrieveTop2(list);//Calling RetrieveTop2
+            RetrieveTeenageRecords(list);//Calling RetrieveTeenageRecords
         }
         //UC1 Adding Details
-        public static void AddingPerson(List<PersonClass> personList)
+        public static void AddingPerson(List<PersonClass> personList)//Adding Method to Add Person Details
         {
-            personList.Add(new PersonClass() { SSN = 1, Age = 16, Name = "Shalu", Address = "Chennai" });
-            personList.Add(new PersonClass() { SSN = 5, Age = 18, Name = "Shivam", Address = "Chennai" });
-            personList.Add(new PersonClass() { SSN = 7, Age = 25, Name = "Shiva", Address = "Chennai" });
-            personList.Add(new PersonClass() { SSN = 2, Age = 85, Name = "Ranu", Address = "Chennai" });
-            personList.Add(new PersonClass() { SSN = 6, Age = 15, Name = "Ajay", Address = "Chennai" });
-            personList.Add(new PersonClass() { SSN = 8, Age = 75, Name = "Nithya", Address = "Chennai" });
+            personList.Add(new PersonClass() { SSN = 1, Age = 16, Name = "Naina", Address = "Pune" });
+            personList.Add(new PersonClass() { SSN = 5, Age = 18, Name = "Nilima", Address = "Pune" });
+            personList.Add(new PersonClass() { SSN = 7, Age = 25, Name = "Ashu", Address = "Pune" });
+            personList.Add(new PersonClass() { SSN = 2, Age = 77, Name = "Snehal", Address = "Pune" });
+            personList.Add(new PersonClass() { SSN = 6, Age = 15, Name = "Anku", Address = "Pune" });
+            personList.Add(new PersonClass() { SSN = 8, Age = 75, Name = "Sneha", Address = "Pune" });
         }
 
-        public static void IterateOverLoop(List<PersonClass> personList)
+        public static void IterateOverLoop(List<PersonClass> personList)//Adding Method to Iterate Person
         {
+            //Create Foreach for PersonClass
             foreach (PersonClass person in personList)
             {
                 Console.WriteLine("Age\t" + person.Age + "Name\t" + person.Name + "\t" + "Address\t" + person.Address);
+                Console.ReadLine();
             }
         }
 
         //UC2 Retrieve top two age less than 60
-        public static void RetrieveTop2(List<PersonClass> list)
+        public static void RetrieveTop2(List<PersonClass> list)//Adding Method to Iterate Person
         {
-            var result = list.FindAll(x => x.Age < 60).OrderBy(x => x.Age).Take(2);
+            var result = list.FindAll(x => x.Age < 60).OrderBy(x => x.Age).Take(2);//Creating Lamda Function x
+            //Creating Foreach loop
             foreach (PersonClass person in result)
             {
                 Console.WriteLine("Age\t" + person.Age + "Name\t" + person.Name + "\t" + "Address\t" + person.Address);
                 Console.ReadLine();
             }
-        } 
-       
+        }
+
+        //UC3 Retrieve age between 13 to 18 or less 
+        public static void RetrieveTeenageRecords(List<PersonClass> list)//Added Method to RetriveTeenAgeRecord
+        {
+            //Initialized Exception
+            try
+            {
+                //For finding Teenage
+                var result = list.FindAll(x => x.Age < 13 && x.Age > 18);
+                foreach (PersonClass person in result)
+                {
+                    Console.WriteLine("Age\t" + person.Age + "Name\t" + person.Name + "\t" + "Address\t" + person.Address);
+                    Console.Read();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.Read();
+            }
+        }
+
     }
 }
